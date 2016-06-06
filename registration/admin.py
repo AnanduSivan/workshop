c4cbf596695dab634af9c526f1d16f28dddabeb4
AnanduSivan <ananducky@gmail.com>
2016-06-06 14:18:34 +0530

    third commit

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from registration.models import *
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(User, UserAdmin)
+
